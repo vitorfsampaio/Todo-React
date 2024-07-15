af62f583bc178f1156b0b3766deb2a5a9189bb94
Vitor <vitorguilherme286@gmail.com>
2024-07-15 11:15:37 -0300

    atualizar formulário pronto

---
+++ TODO/src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+
+import Todo from './components/Todo'
+import TodoForm from './components/TodoForm';
+
+import "./App.css";
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "criar funcionalidade x no sistema",
+      category: "Trabalho",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      text: "Ir pra academia",
+      category: "Pessoal",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      text: "Estudar React",
+      category: "Estudos",
+      isCompleted: false,
+    }
+  ]);
+
+  const addTodo = (text, category) => {
+    const newTodos = [...todos, {
+      id: Math.floor(Math.random() * 1000),
+      text,
+      category,
+      isCompleted: false,
+    }]
+    setTodos(newTodos)
+  }
+
+  return <div className='app'>
+    <h1>Lista de tarefas</h1>
+    <div className="todo-list">
+      {todos.map((todo) => (
+        <Todo key={todo.id} todo={todo} />
+      ))}
+    </div>
+    <TodoForm addTodo={addTodo} />
+  </div>
+}
+
+export default App
